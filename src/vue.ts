a0b9654784abdced399b56114fb145b73258d535
jameswquinn <jameswquinn@me.com>
2019-05-27 23:08:49 +0100

    update

--- src/vue.ts
+++
@@ -1,6 +0,0 @@
-import Vue from "vue";
-import VueRouter from "vue-router";
-import Vuex from "vuex";
-import Moon from "moonjs";
-import Monx from "monx";
-import MoonRouter from "moon-router";
--- webpack.config.js
+++ webpack.config.js
@@ -156,7 +156,15 @@ module.exports = {
       ".svelte",
       ".vue"
     ],
-    mainFields: ["svelte", "browser", "module", "main"]
+    mainFields: ["svelte", "browser", "module", "main"],
+    alias: {
+      react: "preact-compat",
+      "react-dom": "preact-compat",
+      // Not necessary unless you consume a module using `createClass`
+      "create-react-class": "preact-compat/lib/create-react-class",
+      // Not necessary unless you consume a module requiring `react-dom-factories`
+      "react-dom-factories": "preact-compat/lib/react-dom-factories"
+    }
   },
   output: {
     path: path.join(__dirname, "dist"),
